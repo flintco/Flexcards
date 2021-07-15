5eaa8b27e64416b3b0a3098f69a1379dbf6d57c3
flintco <connor.flint16@gmail.com>
2021-07-14 23:10:22 -0500

    Added start command in package.json to run node.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
