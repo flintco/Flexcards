e3a0a641be0950936f2fb8e82cf42f978c8d9598
flintco <connor.flint16@gmail.com>
2021-07-15 20:12:56 -0500

    Added Routes Folder and database config file

---
+++ backend/databaseConfig.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+
+var config = {
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'schema_test'
+};
+
+module.exports = config;
