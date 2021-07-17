469ad6d3a1661da25ad0cd233a4be7a3c557f91c
flintco <connor.flint16@gmail.com>
2021-07-17 12:41:10 -0500

    Added first route

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,11 @@ const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
+var indexRouter = require('./routes/flipCard');
+
 const app = express();
+
+app.use('/', indexRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
