c230c03b1d37361f75db3918618448f9f33006da
flintco <connor.flint16@gmail.com>
2021-07-19 22:36:33 -0500

    Enabled Cors in backend so react and node connect

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,20 @@
 var express = require("express");
+var cors = require("cors");
+
 var app = express();
 
-//Setup Routes
-//var indexRouter = require('./routes/flipCard');
-//app.use('/', indexRouter);
+app.use(cors());
 
 app.get('/', function(req, res, next) {
   res.send('Get request recieved')
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+//Setup Routes
+//var indexRouter = require('./routes/flipCard');
+//app.use('/', indexRouter);
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      result: ""
+    };
+  }
+
+  componentWillMount(){
+    this.firstAPICall();
+  }
+
+  firstAPICall(){
+    fetch("http://localhost:9000")
+    .then(res => res.text())
+    .then(res => this.setState({result: res}));
+  }
+  
+  render(){
+    return (
+      <div>
+        <p>First text</p>
+        <p>{this.state.result}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
