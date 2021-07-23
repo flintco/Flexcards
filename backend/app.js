11325e157e561b7307d3c8589d24b231b3fe5317
flintco <connor.flint16@gmail.com>
2021-07-22 22:41:23 -0500

    Connect to MongoDB in app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,15 @@ var express = require("express");
 var cors = require("cors");
 var routerForFirst = require('./routes/firstRoute');
 
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://dbUser:password@cluster0.00vzk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => console.log('Connected to DB'))
+  .catch((error) => console.log(err));
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
 var app = express();
 
 app.use(cors());
