40be09e16c10db55da2ba10d5267f7d4713d323c
flintco <connor.flint16@gmail.com>
2021-07-25 20:30:12 -0500

    MongoDB is setup in first route

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,6 @@
 var express = require("express");
 var cors = require("cors");
 var routerForFirst = require('./routes/firstRoute');
-let config = require('../backend/DBConfig');
-
-var mongoose = require('mongoose');
-var mongoDB = config;
-mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then((result) => console.log('Connected to DB'))
-  .catch((error) => console.log(err));
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
 var app = express();
 
--- backend/routes/firstRoute.js
+++ backend/routes/firstRoute.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var theRouter = express.Router();
+let config = require('../DBConfig');
+var mongoose = require('mongoose');
 
 theRouter.get('/', function(req, res, next) {
+    //MongoDB
+    var connectionString = config;
+    mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true})
+        .then((result) => console.log('Connected to DB'))
+        .catch((error) => console.log(err));
+
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
     res.send('Get request recieved 2')
 });
 
