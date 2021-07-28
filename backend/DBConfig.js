2b5c54121ee2c34fd7fe33b561ba627c7d4b884b
flintco <connor.flint16@gmail.com>
2021-07-27 23:56:00 -0500

    Created first schema and model

--- backend/DBConfig.js
+++ backend/DBConfig.js
@@ -1,5 +1,5 @@
 var password = 'password';
 
-var config = 'mongodb+srv://dbUser:' + password + '@cluster0.00vzk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+var config = 'mongodb+srv://dbUser:' + password + '@cluster0.00vzk.mongodb.net/Flexcards-Application?retryWrites=true&w=majority';
 
 module.exports = config;
