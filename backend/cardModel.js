0d69c083563674e2449118523afed48dcb0f6b58
flintco <connor.flint16@gmail.com>
2021-08-12 15:58:07 -0500

    Post handler can modify which deck to add card

--- backend/cardModel.js
+++ backend/cardModel.js
@@ -1,5 +1,3 @@
 var mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({name: 'string', front: 'string', back: 'string', hint: 'string'}, {collection: 'Card-Deck'});
-
-module.exports = mongoose.model('Card-Deck', schema);
+module.exports = new mongoose.Schema({name: 'string', front: 'string', back: 'string', hint: 'string'});
--- backend/routes/newCard.js
+++ backend/routes/newCard.js
@@ -2,16 +2,17 @@ var express = require('express');
 var theRouter = express.Router();
 let connectionString = require('../DBConfig');
 var mongoose = require('mongoose');
-var cardModel = require('../cardModel');
+var schema = require('../cardModel');
 
 theRouter.post('/', function(req, res) {
      
-    //Receive JSON from Post and parse
+    //Receive JSON from POST request and parse
     var cardFront = JSON.stringify(req.body.front);
     var cardBack = JSON.stringify(req.body.back);
     var cardHint = JSON.stringify(req.body.hint); 
+    //var cardDeck = JSON.stringify(req.body.deck);
     
-    //MongoDB
+    //Setup MongoDB connection
     mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true})
         .then((result) => console.log('Connected to DB'))
         .catch((error) => console.log(err));
@@ -21,12 +22,15 @@ theRouter.post('/', function(req, res) {
         console.log('Connected')
     })
 
+    //Add card to specified deck
+    var cardDeck = 'History';
+    cardModel = mongoose.model(cardDeck, schema);
     cardModel.create({ front: cardFront, back: cardBack, hint: cardHint}, function (err, medium) {
         if (err) return handleError(err);
         // saved!
     });
 
-    //db.disconnect();
+    //Close DB connection
 
     res.send('New Card route successful')
 });
